86ce03bb947b5ad6e94bf7bc6b4dc3dade0ef392
viprocompany <vitali_igorevich_doc@mail.ru>
2019-02-03 21:18:42 +0300

    09
    адаптированы картинки в хедере и футере под любой экран и картинки в кауселе ограничены максимальным размером

--- js/script.js
+++ js/script.js
@@ -2,5 +2,11 @@
 	interval: 7000
 });
 $('.carousel-fade').carousel({
-	interval: 7000
+	interval: 5000
 });
+$(document).ready(function(){
+ $('.footer').width($(window).width());
+})
+$(document).ready(function(){
+ $('.header').width($(window).width());
+})
